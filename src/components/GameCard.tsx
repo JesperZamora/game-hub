c8b00cc6eb9b13a9e5fe2fbb05265f42bb124e8c
JesperZamora <jesp78m6@stud.kea.dk>
2025-04-05 22:25:52 +0200

    Hover and fade in effect

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,12 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Heading,
+  HStack,
+  Image,
+  SlideFade,
+} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -11,21 +18,34 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card overflow="hidden">
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent={"space-between"} marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize={"2xl"}>
-          {game.name}
-          <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
-    </Card>
+    <SlideFade
+      in={true}
+      offsetY="20px"
+      transition={{ enter: { duration: 0.4 } }}
+    >
+      <Card
+        overflow="hidden"
+        transition="all 0.3s"
+        _hover={{
+          transform: "scale(1.04)",
+          boxShadow: "lg",
+        }}
+      >
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize={"2xl"}>
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </SlideFade>
   );
 };
 
