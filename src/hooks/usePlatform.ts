10119e408306838ac6222e5548656213c6b9d02d
JesperZamora <jesp78m6@stud.kea.dk>
2025-04-09 16:06:24 +0200

    Refactor platforms hook

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,8 +1,9 @@
+import { Platform } from "../entities/Platform";
 import usePlatforms from "./usePlatforms";
 
 const usePlateform = (id?: number) => {
     const { data: platforms } = usePlatforms();
-    return platforms?.results.find((p) => p.id === id);
+    return platforms?.results.find((p: Platform) => p.id === id);
 };
 
 export default usePlateform;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-import platforms from "../data/platforms.ts";
+import platforms from "../data/platforms";
 import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
